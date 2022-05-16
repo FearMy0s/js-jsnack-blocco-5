9f76d076565485a7e48385643891aa880b2abe0a
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-16 12:30:15 +0200

    push

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 const zucchine = [
     {
         varieta: "zucchina 1",
-        peso: 1,
+        peso: 15,
         lunghezza: "1cm",
     },
     {
         varieta: "zucchina 2",
-        peso: 2,
+        peso: 24,
         lunghezza: "2cm",
     },
     {
         varieta: "zucchina 3",
-        peso: 3,
+        peso: 32,
         lunghezza: "3cm",
     },
     {
         varieta: "zucchina 4",
-        peso: 4,
+        peso: 43,
         lunghezza: "4cm",
     },
     {
         varieta: "zucchina 5",
-        peso: 5,
+        peso: 57,
         lunghezza: "5cm",
     },
     {
         varieta: "zucchina 6",
-        peso: 6,
+        peso: 62,
         lunghezza: "6cm",
     },
     {
         varieta: "zucchina 7",
-        peso: 7,
+        peso: 37,
         lunghezza: "7cm",
     },
     {
         varieta: "zucchina 8",
-        peso: 8,
+        peso: 81,
         lunghezza: "8cm",
     },
     {
         varieta: "zucchina 9",
-        peso: 9,
+        peso: 29,
         lunghezza: "9cm",
     },
     {
         varieta: "zucchina 10",
-        peso: 10,
+        peso: 13,
         lunghezza: "10cm",
     },
 ];
